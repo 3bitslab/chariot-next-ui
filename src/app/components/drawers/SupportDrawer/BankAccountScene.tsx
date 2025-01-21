146878a788691ce87e2bce91e926911d9cd234e8
Manickam Murugappan <manickam1999@gmail.com>
2025-01-21 18:40:42 +0800

    feat(i18n): add translations for Support drawer and update BankAccountScene component

--- src/app/components/drawers/SupportDrawer/BankAccountScene.tsx
+++ src/app/components/drawers/SupportDrawer/BankAccountScene.tsx
@@ -3,24 +3,27 @@ import Image from "next/image";
 import React from "react";
 import { toast } from "sonner";
 import Divider from "../../Divider";
+import { useTranslations } from "next-intl";
 
 function BankAccountScene() {
+    const t = useTranslations("Support.scene");
+
     const bankAccountDetails = [
-        { title: "Bank Name", value: "HSBC Bank Malaysia Berhad" },
-        { title: "Account Name", value: "Manickam Murugappan" },
-        { title: "Account Number", value: "371348475025" },
+        { title: t("bankName"), value: "HSBC Bank Malaysia Berhad" },
+        { title: t("accountName"), value: "Manickam Murugappan" },
+        { title: t("accountNumber"), value: "371348475025" },
     ];
 
-    const handleCopyToClipboard = (value: string) => {
+    const handleCopyToClipboard = (value: string, label: string) => {
         navigator.clipboard.writeText(value);
-        toast(`Copied "${value}" to clipboard!`);
+        toast.success(t("copied", { field: label }));
     };
 
     return (
         <div className="flex flex-col gap-y-2 items-center justify-center">
             <div className="flex flex-col items-center gap-y-2">
                 <span className="font-bold text-primary-850 dark:text-primary-100">
-                    Malaysian National QR
+                    {t("malaysiaQR")}
                 </span>
                 <div className="relative p-2 bg-white rounded-lg shadow-lg">
                     <Image
@@ -48,7 +51,7 @@ function BankAccountScene() {
             <Divider />
             <div className="flex flex-col items-center gap-y-2 w-full">
                 <span className="font-bold text-primary-850 dark:text-primary-50">
-                    Bank Transfer Details
+                    {t("detail")}
                 </span>
                 <div className="flex flex-col gap-y-3 rounded-lg bg-primary-100 p-3 w-full md:w-fit md:items-center">
                     {bankAccountDetails.map((detail) => (
@@ -58,12 +61,15 @@ function BankAccountScene() {
                         >
                             <span className="font-bold">{detail.title}: </span>
                             <span>{detail.value}</span>
-                            {["Account Name", "Account Number"].includes(
+                            {[t("accountName"), t("accountNumber")].includes(
                                 detail.title
                             ) && (
                                 <button
                                     onClick={() =>
-                                        handleCopyToClipboard(detail.value)
+                                        handleCopyToClipboard(
+                                            detail.value,
+                                            detail.title
+                                        )
                                     }
                                     className="hover:cursor-pointer"
                                 >
