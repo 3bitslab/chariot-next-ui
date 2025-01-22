c26f81809fd5a8546fafbaf064480b2ba9c382cf
Manickam Murugappan <manickam1999@gmail.com>
2025-01-22 19:38:53 +0800

    feat(ui): enhance Selectables component with improved accessibility and layout

--- src/app/components/shared/Selectables.tsx
+++ src/app/components/shared/Selectables.tsx
@@ -19,18 +19,28 @@ function Selectables({
             className="gap-y-5 md:flex md:w-full md:gap-x-10"
         >
             {items.map((item) => (
-                <div className="flex items-center space-x-2" key={item.title}>
-                    <div>{item.logo}</div>
-                    <div className="flex flex-col px-2 md:px-0">
-                        <span className="text-lg font-semibold text-primary-850 dark:text-primary-50 capitalize">
-                            {item.title}
-                        </span>
-                        <span className="text-sm text-primary-800 dark:text-primary-100">
-                            {item.description}
-                        </span>
+                <label
+                    key={item.title}
+                    htmlFor={item.title}
+                    className="flex items-center justify-between w-full space-x-4 p-3 rounded-md cursor-pointer"
+                >
+                    <div className="flex items-center space-x-2">
+                        <div>{item.logo}</div>
+                        <div className="flex flex-col px-2 md:px-0">
+                            <span className="text-lg font-semibold text-primary-850 dark:text-primary-50 capitalize">
+                                {item.title}
+                            </span>
+                            <span className="text-sm text-primary-800 dark:text-primary-100">
+                                {item.description}
+                            </span>
+                        </div>
                     </div>
-                    <RadioGroupItem value={item.value} id={item.title} />
-                </div>
+                    <RadioGroupItem
+                        value={item.value}
+                        id={item.title}
+                        className="h-5 w-5"
+                    />
+                </label>
             ))}
         </RadioGroup>
     );
