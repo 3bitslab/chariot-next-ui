78088cef608bc14e30a9c12c2f1fce74eb462cb2
Manickam Murugappan <manickam1999@gmail.com>
2025-01-27 19:06:59 +0800

    fix(layout): remove unnecessary viewport meta tag from layout component

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -197,12 +197,6 @@ export default async function RootLayout({
     const messages = await getMessages();
     return (
         <html lang={locale} suppressHydrationWarning>
-            <head>
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-                />
-            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
