30bf65f7eeb79cd928e846469033e67f007caf29
Manickam Murugappan <manickam1999@gmail.com>
2025-01-29 16:32:01 +0800

    feat(vehicle): refactor vehicleAtom to reset checkpoint and roadblock states on update

--- src/atoms/vehicle.ts
+++ src/atoms/vehicle.ts
@@ -1,7 +1,19 @@
+import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { TTrackerType } from "../constants/types";
+import { checkpointAtom } from "./checkpoint";
+import { roadBlockAtom } from "./road-block";
 
-export const vehicleAtom = atomWithStorage<TTrackerType>(
+const baseVehicleAtom = atomWithStorage<TTrackerType>(
     "vehicle-tracker",
     "chariot"
+);
+
+export const vehicleAtom = atom(
+    (get) => get(baseVehicleAtom),
+    (get, set, newValue: TTrackerType) => {
+        set(baseVehicleAtom, newValue);
+        set(checkpointAtom, false);
+        set(roadBlockAtom, false);
+    }
 );
