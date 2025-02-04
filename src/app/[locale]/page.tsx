6073233743c783684b25b61919e46d3c9de1a22c
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-02-04 11:15:42 +0800

    refactor(page): simplify page component by removing PIN verification logic and dialog

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,14 +2,8 @@
 
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
-import { useAtom } from "jotai";
-import { isPinVerifiedAtom } from "@/atoms/pin";
-import { PinDialog } from "@/app/components/shared/PinDialog";
-
-const isProd = process.env.NODE_ENV === "production";
 
 export default function Page() {
-    const [isPinVerified] = useAtom(isPinVerifiedAtom);
     const Map = useMemo(
         () =>
             dynamic(() => import("@/app/components/map/"), {
@@ -18,12 +12,5 @@ export default function Page() {
         []
     );
 
-    const shouldShowMap = !isProd || isPinVerified;
-
-    return (
-        <>
-            {isProd && !isPinVerified && <PinDialog />}
-            {shouldShowMap && <Map posix={[5.41613, 100.33944]} />}
-        </>
-    );
+    return <Map posix={[5.41613, 100.33944]} />;
 }
