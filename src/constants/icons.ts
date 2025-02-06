98323cd37fc5c96547ec30ee49f6deb44b35ff43
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-02-06 16:06:50 +0800

    fix(messages): update Tamil localization for start marker text and adjust icon text size

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -12,7 +12,7 @@ const createEndIcon = (text: string) =>
         html: `
         <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
                     flex items-center justify-center shadow-md" style="background-color: #f3967f">
-            <span class="text-white font-bold text-sm">${text}</span>
+            <span class="text-white font-bold ${text === "முடிவு" ? "text-[10px]" : "text-sm"}">${text}</span>
             <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
                         border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
                         border-t-[12px] border-t-white">
@@ -29,7 +29,7 @@ const createStartIcon = (text: string) =>
         html: `
             <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
                         flex items-center justify-center shadow-md" style="background-color: #5aaa64">
-                <span class="text-white opacity-10 font-bold text-sm">${text}</span>
+                <span class="text-white opacity-10 font-bold ${text === "துவக்கம்" ? "text-[10px]" : "text-sm"}">${text}</span>
                 <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
                             border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
                             border-t-[12px] border-t-white">
