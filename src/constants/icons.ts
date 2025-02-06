f4c36c6fc1d5b635be47611de34ef0c5dc698f57
manickam1999 <manickam1999@gmail.com>
2025-02-06 20:49:07 +0800

    Marker translation (#41)
    * feat(map): add localized markers for start and end points in map component
    
    * fix(messages): update Tamil localization for start marker text
    fix(icons): adjust icon sizes and styles for start and end markers
    
    * fix(messages): update Tamil localization for start marker text and adjust icon text size
    
    ---------
    
    Co-authored-by: Manickam Murugappan <manickam.murugappan@vitrox.com>

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -7,36 +7,38 @@ const chariotIcon = L.divIcon({
     iconAnchor: [24, 24],
 });
 
-const endIcon = L.divIcon({
-    html: `
-        <div class="relative w-[45px] h-[45px] rounded-full border-4 border-white 
+const createEndIcon = (text: string) =>
+    L.divIcon({
+        html: `
+        <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
                     flex items-center justify-center shadow-md" style="background-color: #f3967f">
-            <span class="text-white font-bold text-sm">End</span>
-            <div class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 
-                        border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent 
-                        border-t-[10px] border-t-white">
+            <span class="text-white font-bold ${text === "முடிவு" ? "text-[10px]" : "text-sm"}">${text}</span>
+            <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
+                        border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
+                        border-t-[12px] border-t-white">
             </div>
         </div>
     `,
-    className: "end-marker",
-    iconSize: [45, 45],
-    iconAnchor: [22, 50],
-});
+        className: "end-marker",
+        iconSize: [55, 55],
+        iconAnchor: [27, 60],
+    });
 
-const startIcon = L.divIcon({
-    html: `
-        <div class="relative w-[45px] h-[45px] rounded-full border-4 border-white 
-                    flex items-center justify-center shadow-md" style="background-color: #5aaa64">
-            <span class="text-white opacity-10 font-bold text-sm">Start</span>
-            <div class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 
-                        border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent 
-                        border-t-[10px] border-t-white">
+const createStartIcon = (text: string) =>
+    L.divIcon({
+        html: `
+            <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
+                        flex items-center justify-center shadow-md" style="background-color: #5aaa64">
+                <span class="text-white opacity-10 font-bold ${text === "துவக்கம்" ? "text-[10px]" : "text-sm"}">${text}</span>
+                <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
+                            border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
+                            border-t-[12px] border-t-white">
+                </div>
             </div>
-        </div>
-    `,
-    className: "start-marker",
-    iconSize: [45, 45],
-    iconAnchor: [22, 50],
-});
+        `,
+        className: "start-marker",
+        iconSize: [55, 55],
+        iconAnchor: [27, 60],
+    });
 
-export { chariotIcon, startIcon, endIcon };
+export { chariotIcon, createStartIcon, createEndIcon };
