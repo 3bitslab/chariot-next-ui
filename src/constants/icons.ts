6cab2071a52a5bd74be0607d005c0e915b7c12c7
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-02-06 15:56:38 +0800

    fix(messages): update Tamil localization for start marker text fix(icons): adjust icon sizes and styles for start and end markers

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -10,35 +10,35 @@ const chariotIcon = L.divIcon({
 const createEndIcon = (text: string) =>
     L.divIcon({
         html: `
-        <div class="relative w-[45px] h-[45px] rounded-full border-4 border-white 
+        <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
                     flex items-center justify-center shadow-md" style="background-color: #f3967f">
             <span class="text-white font-bold text-sm">${text}</span>
-            <div class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 
-                        border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent 
-                        border-t-[10px] border-t-white">
+            <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
+                        border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
+                        border-t-[12px] border-t-white">
             </div>
         </div>
     `,
         className: "end-marker",
-        iconSize: [45, 45],
-        iconAnchor: [22, 50],
+        iconSize: [55, 55],
+        iconAnchor: [27, 60],
     });
 
 const createStartIcon = (text: string) =>
     L.divIcon({
         html: `
-        <div class="relative w-[45px] h-[45px] rounded-full border-4 border-white 
-                    flex items-center justify-center shadow-md" style="background-color: #5aaa64">
-            <span class="text-white opacity-10 font-bold text-sm">${text}</span>
-            <div class="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-0 h-0 
-                        border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent 
-                        border-t-[10px] border-t-white">
+            <div class="relative w-[55px] h-[55px] aspect-square rounded-full border-4 border-white 
+                        flex items-center justify-center shadow-md" style="background-color: #5aaa64">
+                <span class="text-white opacity-10 font-bold text-sm">${text}</span>
+                <div class="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 
+                            border-l-[12px] border-l-transparent border-r-[12px] border-r-transparent 
+                            border-t-[12px] border-t-white">
+                </div>
             </div>
-        </div>
-    `,
+        `,
         className: "start-marker",
-        iconSize: [45, 45],
-        iconAnchor: [22, 50],
+        iconSize: [55, 55],
+        iconAnchor: [27, 60],
     });
 
 export { chariotIcon, createStartIcon, createEndIcon };
