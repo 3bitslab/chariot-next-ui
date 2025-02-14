87d74302210b8d687faa2aad9cac632693e3147e
Manickam Murugappan <manickam.murugappan@vitrox.com>
2025-02-14 13:47:38 +0800

    feat: add Financial section to multilingual messages and update DrawerFinanceDrawer component

--- src/app/components/drawers/DrawerFinanceDrawer/DrawerFinanceDrawer.tsx
+++ src/app/components/drawers/DrawerFinanceDrawer/DrawerFinanceDrawer.tsx
@@ -1,11 +1,14 @@
-import { Drawer, DrawerContent } from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerTitle } from "@/components/ui/drawer";
 import React from "react";
 import { useAtom } from "jotai";
 import { drawerAtom } from "@/atoms/drawer";
+import Divider from "../../Divider";
+import { useTranslations } from "next-intl";
 
 function DrawerFinanceDrawer() {
     const [currentDrawer, setCurrentDrawer] = useAtom(drawerAtom);
     const isOpen = currentDrawer?.type === "drawerFinance";
+    const t = useTranslations("Financial");
 
     return (
         <Drawer
@@ -19,7 +22,16 @@ function DrawerFinanceDrawer() {
             }}
         >
             <DrawerContent className="px-3 w-full">
-                <span>Test</span>
+                <div className="py-5 gap-y-3 flex flex-col w-full max-h-[50%] overflow-auto">
+                    <DrawerTitle className="py-3 flex justify-between items-center">
+                        <span className="font-semibold opacity-80 text-primary-800 dark:text-primary-150">
+                            {t("title")}
+                        </span>
+                    </DrawerTitle>
+
+                    {/* Divider */}
+                    <Divider />
+                </div>
             </DrawerContent>
         </Drawer>
     );
